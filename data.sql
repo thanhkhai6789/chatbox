336b6fac6d16437b41d7142dc8b3cd4d0379133a
thanhkhai6789 <65377181+thanhkhai6789@users.noreply.github.com>
2020-05-15 09:54:47 +0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -7,10 +7,6 @@
 -- Phiên bản máy phục vụ: 10.3.16-MariaDB
 -- Phiên bản PHP: 7.3.12
 
-/*!SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO"*/;
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
